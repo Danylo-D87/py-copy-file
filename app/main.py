698d3123963a2935bbae0158d814298230735a89
Danylo-D87 <quaestor88@gmail.com>
2025-04-29 19:15:15 +0300

    sol

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    if not command:
+        return
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+    name1, name2 = parts[1], parts[2]
+    if name1 == name2:
+        return
+    if not os.path.exists(name1):
+        return
+    with open(name1, "r") as f1, open(name2, "w") as f2:
+        f2.write(f1.read())
